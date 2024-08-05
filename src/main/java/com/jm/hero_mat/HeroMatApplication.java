34aab0ccbe00d8b12823cdf0a47eb3e370009fa8
JuliusMatadamas <julius3877@gmail.com>
2024-08-04 18:42:52 -0600

    Inicio del proyecto

---
+++ src/main/java/com/jm/hero_mat/HeroMatApplication.java
@@ -0,0 +1,13 @@
+package com.jm.hero_mat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroMatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroMatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jm/hero_mat/HeroMatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.hero_mat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroMatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
